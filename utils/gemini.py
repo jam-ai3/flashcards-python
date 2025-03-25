35f94ffb860b03c2f8c325d9d8508b9e3adf5167
Matt Linder <108966946+mlinder10@users.noreply.github.com>
2025-03-24 20:23:08 -0400

    loading env file right before gemini config

--- utils/gemini.py
+++ utils/gemini.py
@@ -1,6 +1,10 @@
 import os
 import json
 import google.generativeai as genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 model = genai.GenerativeModel(model_name="gemini-2.0-flash")
