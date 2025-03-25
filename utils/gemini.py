d92d44fc9eade5d3a565784e9d51a98096dd2125
Matt Linder <108966946+mlinder10@users.noreply.github.com>
2025-03-24 20:36:28 -0400

    server test

--- utils/gemini.py
+++ utils/gemini.py
@@ -3,7 +3,7 @@ import json
 import google.generativeai as genai
 from dotenv import load_dotenv
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ENV_PATH = os.path.join(BASE_DIR, ".env")
 
 load_dotenv(ENV_PATH)
