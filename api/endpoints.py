b4bbec36a1aeb67dc68bbca613007ba5badde80b
Matt Linder <108966946+mlinder10@users.noreply.github.com>
2025-03-31 10:19:35 -0400

    restructured generate endpoint to enable client side calling

--- api/endpoints.py
+++ api/endpoints.py
@@ -1,9 +1,6 @@
 # pdf conversion endpoint
-import os
 from flask import json, jsonify, request
 from flask_cors import cross_origin
-import requests
-import threading
 from utils.gemini import generate
 from google.generativeai.types.content_types import BlobDict
 
@@ -42,9 +39,14 @@ class GenerateFlashcardsEndpoint:
             if not input_type or not input_format or not payment_type or not group_id or not user_id:
                 return jsonify({"error": "Missing required fields"}), 400
 
-            create_thread(self.generate_and_send_flashcards, group_id, user_id, input_type,
-                          input_format, payment_type, text, image, pdf, pptx, course_info)
-            return jsonify({"message": "Flashcards generation started"}), 200
+            text, file = self.format_gemini_input(
+                text, image, pdf, pptx, course_info
+            )
+
+            flashcards = generate(input_type, text, file,
+                                  is_free=payment_type == "free")
+
+            return jsonify(flashcards), 200
 
     def format_gemini_input(self, text, image, pdf, pptx, course_info):
         file = None
@@ -58,29 +60,3 @@ class GenerateFlashcardsEndpoint:
         if course_info:
             text = course_info
         return text, file
-
-    def generate_and_send_flashcards(self, group_id, user_id, input_type, input_format,
-                                     payment_type, text, image, pdf, pptx, course_info):
-        text, file = self.format_gemini_input(
-            text, image, pdf, pptx, course_info)
-        flashcards = generate(input_type, text, file,
-                              is_free=payment_type == "free")
-        body = {
-            "flashcards": flashcards,
-            "groupId": group_id,
-            "userId": user_id,
-            "inputType": input_type,
-            "inputFormat": input_format,
-            "paymentType": payment_type,
-            "prompt": text if text else course_info if course_info else "Flashcards generated with a file",
-        }
-        requests.post(
-            f"{os.environ['NODE_SERVER_URL']}/api/flashcards",
-            json=body
-        )
-
-
-def create_thread(func, *args, **kwargs):
-    thread = threading.Thread(target=func, args=args, kwargs=kwargs)
-    thread.start()
-    return thread
